2c4d70121cd8fa7bf52846c504f8fea0dda8206b
renzw <renzw@enlink.cn>
2024-02-06 14:23:13 +0800

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Put } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('app')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Put('list/user')
+  updateUser() {
+    return { userId: 1 };
+  }
+
+  @Put('list/:id')
+  update() {
+    return "update";
+  }
+
+  
+}
