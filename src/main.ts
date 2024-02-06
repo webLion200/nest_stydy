377b07b43d983cd02094e75ecaf66fa545e120b8
renzw <renzw@enlink.cn>
2024-02-06 14:27:29 +0800

    首次提交

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './core/filter/http-exception/http-exception.filter';
+import { TransformInterceptor } from './core/interceptor/transform/transform.interceptor';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api')
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api');
+  // 注册全局错误的过滤器
+
+  app.useGlobalPipes(new ValidationPipe())
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('管理后台')
+    .setDescription('管理后台接口文档')
+    .setVersion('1.0')
+    .addTag('cats')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+
   await app.listen(3000);
 }
 bootstrap();
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { PostsService, PostsRo } from './posts.service';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dot';
 
-@Controller('posts')
-export class PostsController {}
+@ApiTags("文章")
+@Controller('post')
+export class PostsController {
+    constructor(private readonly postsService: PostsService) {}
+
+    @ApiOperation({summary: '创建文章'})
+    @Post()
+    async create(@Body() post: CreatePostDto) {
+        return await this.postsService.create(post)
+    }
+
+    @ApiOperation({summary: '获取文章列表'})
+    @Get() 
+    async findAll(@Query() query): Promise<PostsRo> {
+        return await this.postsService.findAll(query)
+    }
+
+    @ApiOperation({summary: '查询文章'})
+    @Get(':id')
+    async findById(@Param('id') id) {
+        return await this.postsService.findById(id)
+    }
+
+    @ApiOperation({summary: '更新文章'})
+    @Put(":id")
+    async update(@Param('id') id, @Body() post) {
+        return await this.postsService.updateById(id, post)
+    }
+
+    @ApiOperation({summary: '删除文章'})
+    @Delete(":id")
+    async remove(@Param("id") id){
+        return await this.postsService.remove(id)
+    }
+}
